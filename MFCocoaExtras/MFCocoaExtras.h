5b309897a6e806f843288834afa581d9df6e8705
Tristan Leblanc <tristan@moosefactory.eu>
2016-03-14 21:02:49 +0100

    Initial Commit

---
+++ MFCocoaExtras/MFCocoaExtras.h
@@ -0,0 +1,19 @@
+//
+//  MFCocoaExtras.h
+//  MFCocoaExtras
+//
+//  Created by Tristan Leblanc on 14/03/16.
+//  Copyright © 2016 MooseFactory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MFCocoaExtras.
+FOUNDATION_EXPORT double MFCocoaExtrasVersionNumber;
+
+//! Project version string for MFCocoaExtras.
+FOUNDATION_EXPORT const unsigned char MFCocoaExtrasVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MFCocoaExtras/PublicHeader.h>
+
+
