a19508525a72a2dee20b1f29b90c3264653a1969
Tristan Leblanc <tristan@moosefactory.eu>
2016-03-14 22:00:12 +0100

    Added MIT license, pod and AppleDocs support

---
+++ MFCocoaExtras.podspec
@@ -0,0 +1,31 @@
+#
+# Be sure to run `pod lib lint MFCocoaExtras.podspec' to ensure this is a
+# valid spec before submitting.
+#
+# Any lines starting with a # are optional, but their use is encouraged
+# To learn more about a Podspec see http://guides.cocoapods.org/syntax/podspec.html
+#
+
+Pod::Spec.new do |s|
+  s.name             = "MFCocoaExtras"
+  s.version          = "1.0.0"
+  s.summary          = "MFCocoaExtras is a cross-platofrm (iOS/MacOS) Utility Library."
+  s.description      = <<-DESC
+                    MFCocoaExtras is a cross-platofrm (iOS/MacOS) Utility Library build on Cocoa or CocoaTouch (UIKit) frameworks.
+                    It includes
+                    - Convenient extras ( categories ) on some Cocoa classes
+                    - Graphics Utilities
+                    - Security
+                       DESC
+  s.homepage         = "https://github.com/moosefactory/MFCocoaExtras"
+  s.license          = 'MIT'
+  s.author           = { "Tristan Leblanc" => "tristan@moosefactory.eu" }
+  s.source           = { :git => "https://github.com/moosefactory/MFCocoaExtras.git", :tag => s.version.to_s }
+  s.social_media_url = 'https://twitter.com/moosefactory_eu'
+
+    s.ios.deployment_target = '6.0'
+    s.osx.deployment_target = '10.8'
+    s.requires_arc = true
+
+  s.source_files = 'MFCocoaExtras/**/*'
+end
--- MFCocoaExtras/Sources/CocoaTouch+Extras/UIButton+Extras.m
+++ MFCocoaExtras/Sources/CocoaTouch+Extras/UIButton+Extras.m
@@ -1,10 +1,31 @@
-//
-//  UIControl-Extras.m
-//  EyePhone
-//
-//  Created by Tristan Leblanc on 28/12/10.
-//  Copyright 2010 MooseFactory. All rights reserved.
-//
+/*--------------------------------------------------------------------------*/
+/*   /\/\/\__/\/\/\        MooseFactory Foundation - v1.0.0                 */
+/*   \/\/\/..\/\/\/                                                         */
+/*        |  |             (c)2007-2016 Tristan Leblanc                     */
+/*        (oo)             tristan@moosefactory.eu                          */
+/* MooseFactory Software                                                    */
+/*--------------------------------------------------------------------------*/
+/*
+ **Copyright (c) 2016 Tristan Leblanc - MooseFactory Software <tristan@moosefactory.eu>**
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
 
 #import "UIButton+Extras.h"
 
@@ -15,12 +36,22 @@
 
 -(NSString*)title
 {
-	return [self titleForState:UIControlStateNormal];
+    return [self titleForState:self.state];
+}
+
+-(UIImage*)title
+{
+    return [self imageForState:self.state];
 }
 
 -(void)setTitle:(NSString*)title
 {
-	return [self setTitle:title forState:UIControlStateNormal];
+    return [self setTitle:title forState:self.state];
+}
+
+-(void)setImage:(UIImage*)image
+{
+    [self setImage:image forState:self.state];
 }
 
 -(void)setTitleColorForAllStates:(UIColor*)color
